1646aa9f5aa3cf9a7ca5cf306f771b4ad77a74d5
Joshua Tsang <14105275+Etzeo@users.noreply.github.com>
2021-04-24 04:40:49 +1000

    refactor: convert JavaScript pages into TypeScript

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
