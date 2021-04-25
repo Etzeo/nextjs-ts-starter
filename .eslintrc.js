c5fe3106afaf8fa90a0acabdacb90570efee7e71
Joshua Tsang <14105275+Etzeo@users.noreply.github.com>
2021-04-26 01:28:49 +1000

    chore: update eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,84 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:unicorn/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'unicorn/import-index': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/custom-error-definition': 'error',
+    'unicorn/no-unsafe-regex': 'error',
+    'unicorn/filename-case': [
+      'error',
+      { cases: { camelCase: true, pascalCase: true, kebabCase: true } },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      env: {
+        browser: true,
+        es2021: true,
+        node: true,
+      },
+      extends: [
+        'plugin:react/recommended',
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+      ],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 12,
+        project: './tsconfig.json',
+      },
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/require-default-props': 'off',
+      },
+    },
+    {
+      files: ['components/**/*.ts', 'components/**/*.tsx'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            cases: { pascalCase: true },
+          },
+        ],
+      },
+    },
+    {
+      files: ['pages/**/*.ts', 'pages/**/*.tsx'],
+      rules: {
+        'unicorn/filename-case': [
+          'error',
+          {
+            cases: { kebabCase: true },
+          },
+        ],
+      },
+    },
+    {
+      files: ['*rc.js', '*.config.js'],
+      rules: {
+        'unicorn/prefer-module': 'off',
+      },
+    },
+  ],
+};
